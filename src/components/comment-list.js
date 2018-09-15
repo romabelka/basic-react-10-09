e93d19d5d0ad4d094ce24ff7eacd9008fe602284
nikolay_solodkiy <nicolay.solodkiy@gmail.com>
2018-09-15 15:55:54 +0300

    added close button for comments list

--- src/components/comment-list.js
+++ src/components/comment-list.js
@@ -2,21 +2,37 @@ import React, { Component } from 'react'
 import Comment from './comment'
 
 class CommentList extends Component {
+  state = {
+    isOpenComments: false
+  }
   render() {
     const { comments } = this.props
 
     return (
       <div>
-        {comments.map((item) => (
-          <Comment
-            key={item.id}
-            id={item.id}
-            user={item.user}
-            text={item.text}
-          />
-        ))}
+        {comments && (
+          <button onClick={this.handleBtnClickComments.bind(this)}>
+            {this.state.isOpenComments ? 'close comments' : 'open comments'}
+          </button>
+        )}
+        {comments &&
+          this.state.isOpenComments &&
+          comments.map((item) => (
+            <Comment
+              key={item.id}
+              id={item.id}
+              user={item.user}
+              text={item.text}
+            />
+          ))}
       </div>
     )
+  }
+
+  handleBtnClickComments = () => {
+    this.setState({
+      isOpenComments: !this.setState.isOpenComments
+    })
   }
 }
 
