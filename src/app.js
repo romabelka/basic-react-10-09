52271398fce3221ca950cea7301ed072ad9480ab
Ivan Sheychuk <i.sheychuk@gmail.com>
2018-09-16 09:58:13 +0300

    HT1.2 - Day Picker.

--- src/app.js
+++ src/app.js
@@ -5,21 +5,56 @@ import ArticleList from './components/article-list'
 import ArticlesChart from './components/articles-chart'
 import articles from './fixtures'
 import UserForm from './components/user-form'
+import DayPicker, { DateUtils } from 'react-day-picker'
+import 'react-day-picker/lib/style.css'
 
 class App extends Component {
+  static defaultProps = {
+    numberOfMonths: 2
+  }
+
   state = {
-    openItem: null
+    openItem: null,
+    from: null,
+    to: null
   }
 
   render() {
+    const { from, to } = this.state
+    const modifiers = { start: from, end: to }
+
     return (
       <div>
         <UserForm />
+        <hr />
+        <p>
+          {!from && !to && 'Please select the first day.'}
+          {from && !to && 'Please select the last day.'}
+          {from &&
+            to &&
+            `Selected from ${from.toLocaleDateString()} to
+                ${to.toLocaleDateString()}`}{' '}
+          {from &&
+            to && (
+              <button className="link" onClick={this.handleResetClick}>
+                Reset
+              </button>
+            )}
+        </p>
+        <DayPicker
+          className="Selectable"
+          numberOfMonths={this.props.numberOfMonths}
+          selectedDays={[from, { from, to }]}
+          modifiers={modifiers}
+          onDayClick={this.handleDayClick}
+        />
+        <hr />
         <Select
           options={this.options}
           value={this.state.openItem}
           onChange={this.handleSelect}
         />
+        <hr />
         <ArticleList articles={articles} ref={this.setArticleListRef} />
         <ArticlesChart articles={articles} />
       </div>
@@ -34,6 +69,18 @@ class App extends Component {
   }
 
   handleSelect = (openItem) => this.setState({ openItem })
+
+  handleDayClick = (day) => {
+    const range = DateUtils.addDayToRange(day, this.state)
+    this.setState(range)
+  }
+
+  handleResetClick = () => {
+    this.setState({
+      from: null,
+      to: null
+    })
+  }
 
   setArticleListRef = (ref) => {
     console.log('---', ref, findDOMNode(ref))
