551bb5685909eaef1f879282cd5473199be2f8d9
Artem Ulko <ulkoart@gmail.com>
2018-09-19 08:22:37 +0300

    upd to Master romabelka

--- src/components/article-list.js
+++ src/components/article-list.js
@@ -1,27 +1,41 @@
 import React, { Component } from 'react'
-import Article from './article'
-import accordion from '../decorators/accordion'
+import Comment from './comment'
+import toggleOpen from '../decorators/toggleOpen'
 
-class ArticleList extends Component {
-  render() {
-    return <ul>{this.body}</ul>
+class CommentList extends Component {
+  static defaultProps = {
+    comments: []
   }
 
-  get body() {
-    const { toggleOpenItem, openItemId, articles } = this.props
-    return articles.map((article, comments) => (
-      <li key={article.id}>
-        <Article
-          article={article}
-          comments={article.comments}
-          isOpen={openItemId === article.id}
-          toggleOpen={toggleOpenItem}
-        />
-      </li>
-    ))
+  render() {
+    const { isOpen, toggleOpen } = this.props
+    const text = isOpen ? 'hide comments' : 'show comments'
+    return (
+      <div>
+        <button onClick={toggleOpen}>{text}</button>
+        {this.getBody()}
+      </div>
+    )
+  }
+
+  getBody() {
+    const { comments, isOpen } = this.props
+    if (!isOpen) return null
+
+    const body = comments.length ? (
+      <ul>
+        {comments.map((comment) => (
+          <li key={comment.id}>
+            <Comment comment={comment} />
+          </li>
+        ))}
+      </ul>
+    ) : (
+      <h3>No comments yet</h3>
+    )
+
+    return <div>{body}</div>
   }
 }
 
-const ArticleListWithAccordion = accordion(ArticleList)
-
-export default ArticleListWithAccordion
+export default toggleOpen(CommentList)
