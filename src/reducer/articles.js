e7110fee8ab97981b6679b3325736ae209f37003
Dmytro Zuienko <dimazuien@gmail.com>
2018-09-26 23:28:34 +0300

    Rewrite articles with normalized structure

--- src/reducer/articles.js
+++ src/reducer/articles.js
@@ -1,26 +1,33 @@
 import { DELETE_ARTICLE, ADD_COMMENT_TO_ARTICLE } from '../constants'
-import { normalizedArticles as defaultArticles } from '../fixtures'
+import { normalizedArticles } from '../fixtures'
+
+const defaultArticles = normalizedArticles.reduce(
+  (acc, article) => ({ ...acc, [article.id]: article }),
+  {}
+)
 
 export default (articlesState = defaultArticles, action) => {
   const { type, payload } = action
 
   switch (type) {
     case DELETE_ARTICLE:
-      return articlesState.filter((article) => article.id !== payload.id)
+      return Object.values(articlesState)
+        .filter((article) => article.id !== payload.id)
+        .reduce((acc, article) => ({ ...acc, [article.id]: article }), {})
 
     case ADD_COMMENT_TO_ARTICLE:
-      const currentArticle = articlesState.find(
-        (article) => article.id === payload.articleId
-      )
+      const currentArticle = articlesState[payload.articleId]
       const currentArticleUpdated = {
         ...currentArticle,
         comments: [...currentArticle.comments, payload.commentId]
       }
 
-      return articlesState.map(
-        (article) =>
-          article !== currentArticle ? article : currentArticleUpdated
-      )
+      return Object.values(articlesState)
+        .map(
+          (article) =>
+            article !== currentArticle ? article : currentArticleUpdated
+        )
+        .reduce((acc, article) => ({ ...acc, [article.id]: article }), {})
 
     default:
       return articlesState
